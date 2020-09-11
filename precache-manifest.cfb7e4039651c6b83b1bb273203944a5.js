0c4f6a45d540eec0ee12921361d8796034431135
sahidhossen <sahidhossenbd@gmail.com>
2020-09-11 16:43:31 +0600

    Updates

---
+++ precache-manifest.cfb7e4039651c6b83b1bb273203944a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3224ec1bc1115df6cb1648c17d2205d5",
+    "url": "/widget-page/index.html"
+  },
+  {
+    "revision": "44f549a3af38b1fb7b9c",
+    "url": "/widget-page/static/css/main.f748c399.chunk.css"
+  },
+  {
+    "revision": "94d8dcb6b8223a390d11",
+    "url": "/widget-page/static/js/2.4d442676.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/widget-page/static/js/2.4d442676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f549a3af38b1fb7b9c",
+    "url": "/widget-page/static/js/main.60760435.chunk.js"
+  },
+  {
+    "revision": "40507ef8dc38591b3c97",
+    "url": "/widget-page/static/js/runtime-main.446033d0.js"
+  }
+]);
